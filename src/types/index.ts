dfaaa91e64a15f2c1e7ed9e71249ba5c92d8efaa
khaled-farahat <84389471+khaled-farahat@users.noreply.github.com>
2023-10-09 08:30:48 +0300

    first commit

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export type Booking = {
+  created_at: string;
+  startDate: string;
+  endDate: string;
+  cabinId: number;
+  guestId: number;
+  hasBreakfast: boolean;
+  observations: string;
+  isPaid: boolean;
+  numGuests: number;
+};
+
+export type Cabin = {
+  name: string;
+  maxCapacity: number;
+  regularPrice: number;
+  discount: number;
+  image: string;
+  description: string;
+};
+
+export type Guest = {
+  // id: string;
+  fullName: string;
+  email: string;
+  nationality: string;
+  nationalID: string;
+  countryFlag: string;
+};

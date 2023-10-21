a8839525d878556a7a0df40bac55ac17ceee6ab7
khaled-farahat <84389471+khaled-farahat@users.noreply.github.com>
2023-10-21 21:47:59 +0300

    Add error boundary

--- src/ui/ErrorFallback.tsx
+++ src/ui/ErrorFallback.tsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { FallbackProps } from "react-error-boundary";
+import Heading from "./Heading";
+import GlobalStyles from "@/styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +33,22 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 🧐</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+};
+
+export default ErrorFallback;
